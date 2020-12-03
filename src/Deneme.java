2a3f366bc4bf197b0ca25e10a4ea2e5135214306
Leyla Kandemir <leylakandemir00@gmail.com>
2020-12-03 22:11:22 +0100

    feature 2

--- src/Deneme.java
+++ src/Deneme.java
@@ -5,5 +5,8 @@ public class Deneme {
         System.out.println("Feature branchten 1. yazimiz");
         System.out.println("Feature branchten 2. yazimiz");
         System.out.println("Github master branchtan 1. yazimiz");
+        System.out.println("Feature branchten tekrar ekleme yaptik");
+        System.out.println("Asla ama asla master branchte degisiklik yapma");
+        System.out.println("Eger yaparsan cok buyuk conflict olusur");
     }
 }
