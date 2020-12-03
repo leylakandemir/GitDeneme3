1939dc399aa263d48b73b1256f173f7d37b17f33
Leyla Kandemir <leylakandemir00@gmail.com>
2020-12-03 21:44:23 +0100

    feature ilk commit

--- src/Deneme.java
+++ src/Deneme.java
@@ -2,6 +2,7 @@
 
 public class Deneme {
     public static void main(String[] args) {
-
+        System.out.println("Feature branchten 1. yazimiz");
+        System.out.println("Feature branchten 2. yazimiz");
     }
 }
