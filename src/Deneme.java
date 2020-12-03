9e90da36094b83532bd46b238c4d8deb4b1377ba
leylakandemir <62206209+leylakandemir@users.noreply.github.com>
2020-12-03 22:01:27 +0100

    Update Deneme.java

--- src/Deneme.java
+++ src/Deneme.java
@@ -4,5 +4,6 @@ public class Deneme {
     public static void main(String[] args) {
         System.out.println("Feature branchten 1. yazimiz");
         System.out.println("Feature branchten 2. yazimiz");
+        System.out.println("Github master branchtan 1. yazimiz");
     }
 }
